fc1b0beb7882fd74122787b6b3cc22875ebbd780
mehranathon <mehran.khmn@gmail.com>
2025-04-26 17:24:16 -0700

    init

---
+++ src/main/java/com/poc/parse_xlsx/ParseXlsxApplication.java
@@ -0,0 +1,13 @@
+package com.poc.parse_xlsx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParseXlsxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParseXlsxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/parse_xlsx/ParseXlsxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.parse_xlsx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParseXlsxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
